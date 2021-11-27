d5472a80aacd869baa513927e3f3a68d7a766a2d
Shinku <ink39x@gmail.com>
2021-11-27 22:46:57 +0800

    feat: book-item styles

--- src/components/book-item/index.tsx
+++ src/components/book-item/index.tsx
@@ -1,9 +1,19 @@
+import { FC } from "react";
 import styles from "./index.module.css";
 
-export const BookItem = () => {
+interface BookItemProps {
+  src: string; // url
+}
+
+export const BookItem: FC<BookItemProps> = (props) => {
+  const { src } = props;
   return (
     <div className={styles["root"]}>
-      <img />
+      <div className={styles["image"]}>
+        <span>10</span>
+        <img src={src} />
+      </div>
+      <span className={styles["title"]}>最後までよろしくお願いします</span>
     </div>
   );
 };
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,14 @@
 import { BookItem } from "components/book-item";
+import demo from "assets/demo.jpg";
 import styles from "./index.module.css";
 
 export const Home = () => {
+  const mock = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   return (
-    <div>
-      <BookItem />
+    <div className={styles["book-list"]}>
+      {mock.map(() => {
+        return <BookItem src={demo} />;
+      })}
     </div>
   );
 };
