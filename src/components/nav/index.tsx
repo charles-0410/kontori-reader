d17128f3d3cab59c20ddaf38e2b720f885bbb6df
charles0410 <446450060@qq.com>
2021-12-02 22:18:56 +0800

    header&navbar开发

--- src/components/nav/index.tsx
+++ src/components/nav/index.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import styles from './index.module.css'
-import { Icon } from '@iconify/react'
-import clsx from 'clsx'
+import React from "react"
+import styles from "./index.module.css"
+import { Icon } from "@iconify/react"
+import clsx from "clsx"
 
 export const Nav = () => {
   return (
-    <div className={styles['nav']}>
-      <div className={styles['nav-container']}>
-        <div className={clsx(styles['nav-item'], styles['active'])}>
+    <div className={styles["nav"]}>
+      <div className={styles["nav-container"]}>
+        <div className={clsx(styles["nav-item"], styles["active"])}>
           <Icon icon="bx:bxs-book-add" fontSize="22" />
           <span>书架</span>
         </div>
-        <div className={styles['nav-item']}>
+        <div className={styles["nav-item"]}>
           <Icon icon="fluent:history-16-filled" fontSize="22" />
           <span>历史</span>
         </div>
-        <div className={styles['nav-item']}>
+        <div className={styles["nav-item"]}>
           <Icon icon="carbon:user-avatar-filled" fontSize="22" />
           <span>我的</span>
         </div>
