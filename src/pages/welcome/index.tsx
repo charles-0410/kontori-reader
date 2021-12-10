e9cd35017b232cbf19cdd1f47d9a5a9dcec29a1e
charles0410 <446450060@qq.com>
2021-12-10 23:10:46 +0800

    refactor: welcome page

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -5,47 +5,45 @@ import { Icon } from '@iconify/react'
 export const Welcome = () => {
   return (
     <div className={styles['welcome']}>
-      <div className={styles['welcome-warp']}>
+      <div className={styles['welcome-wrap']}>
         <div className={styles['header']}>
-          <div className={styles['header-wrap']}>
-            <div className={styles['logo-box']}>
-              <div className={styles['logo-icon']}>
-                <Icon icon="bx:bxs-cube" fontSize="30" />
-              </div>
-              <span>Kontori Reader</span>
+          <div className={styles['header-logo']}>
+            <div className={styles['logo-icon']}>
+              <Icon icon="bx:bxs-cube" fontSize="30" />
             </div>
-            <div className={styles['language-box']}>
-              A/あ <span>English</span>
-            </div>
+            <span>Kontori Reader</span>
+          </div>
+          <div className={styles['language-box']}>
+            A/あ <span>English</span>
           </div>
         </div>
         <div className={styles['container']}>
-          <h1>Welcome!</h1>
-          <h5>What is This?</h5>
-          <h5>This is an open source novel reader that you will love</h5>
-          <div className={styles['container-middle']}>
-            <div className={styles['left-box']}>
-              <div className={styles['left-box-title']}>You Can</div>
-              <div className={styles['link-list']}>
-                <div className={styles['link-item']}>
+          <h1>Welcome !</h1>
+          <h4>What is This ?</h4>
+          <h4>This is an open source novel reader that you will love </h4>
+          <div className={styles['download-box']}>
+            <div className={styles['download-title']}>You Can</div>
+            <div className={styles['download-main']}>
+              <div className={styles['download-list']}>
+                <div className={styles['list-item']}>
                   <Icon icon="fontisto:earth" fontSize="24" />
                   <a href="#">Enter the web client</a>
                 </div>
-                <div className={styles['link-item']}>
+                <div className={styles['list-item']}>
                   <Icon icon="ic:outline-android" fontSize="24" />
                   <a href="#">Download android app</a>
                 </div>
-                <div className={styles['link-item']}>
+                <div className={styles['list-item']}>
                   <Icon icon="ci:apple" fontSize="24" />
                   <a href="#">Download ios app</a>
                 </div>
               </div>
-            </div>
-            <div className={styles['right-box']}>
-              <Icon icon="bx:bxs-cube" fontSize="230" />
+              <div className={styles['download-icon']}>
+                <Icon icon="bx:bxs-cube" fontSize="230" />
+              </div>
             </div>
           </div>
-          <div className={styles['container-bottom']}>
+          <div className={styles['tips']}>
             In addition, you can also view our project code on
             <Icon
               icon="bx:bxl-github"
@@ -56,25 +54,20 @@ export const Welcome = () => {
           </div>
         </div>
         <div className={styles['footer']}>
-          <div className={styles['footer-wrap']}>
-            <div className={styles['icon-box']}>
-              <div className={styles['icon-item']}>
-                <Icon icon="entypo-social:twitter-with-circle" fontSize="28" />
-              </div>
-              <div className={styles['icon-item']}>
-                <Icon icon="entypo-social:facebook-with-circle" fontSize="28" />
-              </div>
-              <div className={styles['icon-item']}>
-                <Icon
-                  icon="entypo-social:instagram-with-circle"
-                  fontSize="28"
-                />
-              </div>
+          <div className={styles['icon-box']}>
+            <div className={styles['icon-item']}>
+              <Icon icon="entypo-social:twitter-with-circle" fontSize="28" />
             </div>
-            <div className={styles['text-box']}>
-              If you like this app, you can share it with others This will drive
-              us to make it better, thank you.
+            <div className={styles['icon-item']}>
+              <Icon icon="entypo-social:facebook-with-circle" fontSize="28" />
             </div>
+            <div className={styles['icon-item']}>
+              <Icon icon="entypo-social:instagram-with-circle" fontSize="28" />
+            </div>
+          </div>
+          <div className={styles['text-box']}>
+            If you like this app, you can share it with others This will drive
+            us to make it better, thank you.
           </div>
         </div>
       </div>
