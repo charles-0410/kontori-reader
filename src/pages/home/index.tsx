db9f7841d434b85bac07ca3df71198944718add5
Shinku <ink39x@gmail.com>
2021-12-19 18:52:19 +0800

    fix: book-list margin & height

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,16 @@
-import { BookItem } from "components/book-item";
-import demo from "assets/demo.jpg";
-import styles from "./index.module.css";
+import { BookItem } from "components/book-item"
+import demo from "assets/demo.jpg"
+import styles from "./index.module.css"
 
 export const Home = () => {
-  const mock = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const mock = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   return (
-    <div className={styles["book-list"]}>
-      {mock.map(() => {
-        return <BookItem src={demo} />;
-      })}
-    </div>
-  );
-};
+    <main className={styles["main"]}>
+      <div className={styles["book-list"]}>
+        {mock.map(() => {
+          return <BookItem src={demo} />
+        })}
+      </div>
+    </main>
+  )
+}
